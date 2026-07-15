0707f3414886591be2de4e3212580d06b3ffe994
Pujan Prajapati <pujan.praj2003@gmail.com>
2026-07-15 06:52:58 +0545

    update

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,3 +1,7 @@
+-- disable ctrl+space treesitter selection to free it for autocomplete
+vim.keymap.del("n", "<C-Space>", { silent = true })
+vim.keymap.del("x", "<C-Space>", { silent = true })
+
 return {
   "hrsh7th/nvim-cmp",
   dependencies = {
@@ -6,14 +10,12 @@ return {
   },
   opts = function(_, opts)
     local cmp = require("cmp")
-
     opts.sources = cmp.config.sources({
       { name = "nvim_lsp", priority = 1000 },
-      { name = "luasnip", priority = 750 },
-      { name = "buffer", priority = 500 },
-      { name = "path", priority = 250 },
+      { name = "luasnip",  priority = 750 },
+      { name = "buffer",   priority = 500 },
+      { name = "path",     priority = 250 },
     })
-
     opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
       ["<CR>"] = cmp.mapping(function(fallback)
         if cmp.visible() and cmp.get_active_entry() then
@@ -22,6 +24,7 @@ return {
           fallback()
         end
       end),
+      ["<C-Space>"] = cmp.mapping.complete(),
     })
   end,
 }
